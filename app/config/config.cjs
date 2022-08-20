ca6d7e833b571b1fd59d335a1485a09d2f70a7b4
Yaroslav <koshkarevj2105@gmail.com>
2022-08-20 21:53:13 +0300

    wip

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -7,11 +7,11 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    database: 'postgres',
+    username: 'doadmin',
+    password: 'AVNS_wWU2usRBxpPgldJIYwi',
+    port: 25060,
+    host: 'db-postgresql-fra1-48994-do-user-11933137-0.b.db.ondigitalocean.com',
     dialectOptions: {
       ssl: {
         require: true,
